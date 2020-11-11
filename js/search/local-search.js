6c47af051e4b04799344e204bb7f4480890b8743
Thinksky5124 <thinksky@mail.dlut.edu.cn>
2020-11-11 16:54:08 +0800

    Site updated: 2020-11-11 16:54:08

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -55,7 +55,7 @@ $(function () {
 
   function search (path) {
     $.ajax({
-      url: GLOBAL_CONFIG.root + path,
+      url: "https://cdn.jsdelivr.net/gh/Thinksky5124/Thinksky5124.github.io/search.xml",
       dataType: 'xml',
       success: function (xmlResponse) {
         // get the contents from search data
